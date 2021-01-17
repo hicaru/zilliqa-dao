62ffbef0bb2a8d2396ea1bb3737852e3f212eea2
Rinat <lich666black@gmail.com>
2021-01-17 18:51:17 +0500

    added schedule

--- bot/index.js
+++ bot/index.js
@@ -1,1 +1,13 @@
 require('dotenv').config();
+const schedule = require('node-schedule');
+
+const privateKey = process.env.PRIVATE_KEY;
+const stakeManage = process.env.STAKE_MANAGER;
+
+function main() {}
+
+main();
+
+schedule.scheduleJob('0 17 ? * 0,4-6', function(){
+  main();
+});
